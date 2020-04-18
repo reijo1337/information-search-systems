0ddd5f452e581155c1660a9c64790dadfd179fce
Grigory Tantsevov <tantsevov@gmail.com>
2020-04-18 22:22:14 +0300

    `

--- cmd/also/main.go
+++ cmd/also/main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(finiteAutomationMatcher(t, p))
 	fmt.Println(kmpMatcher(t, p))
 	fmt.Println(bmMatcher(t, p))
-	t, p = "00001010110001001", "0001"
+	t, p = "Today is a good day", "day"
 	fmt.Println(t, p)
 	fmt.Println(nativeStringMatcher(t, p))
 	fmt.Println(rabinKarpMathcer(t, p))
